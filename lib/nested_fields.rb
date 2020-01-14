7815173a25d1bbe29a1753ec8eb70f0b9698c585
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-01-14 13:34:49 +0900

    Fix non-unique id warning

--- lib/nested_fields.rb
+++ lib/nested_fields.rb
@@ -24,7 +24,7 @@ module ActionView::Helpers
 
       output << @template.content_tag(:template, id: "#{name}_nested_fields_template") do
         @template.content_tag(:div, class: "#{name}_nested_fields") do
-          @template.fields_for("#{object_name}[#{name}_attributes][_]", object, options, &block)
+          @template.fields_for("#{object_name}[#{name}_attributes][__index__]", object, options, &block)
         end
       end
 
